6435112b827276e5cc1fc7a8c1605d7e960cf018
MALIK Rajat <rajat.malik@soprasteria.com>
2025-10-30 17:28:03 +0530

    MongoDB cluster db configured

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   email: { type: String, required: true, unique: true, index: true },
-  passwordHash: { type: String, required: true },
+  passwordHash: { type: String },
   role: { type: String,enum: ["player", "admin", "host"], default : "player" }
 }, { timestamps: true });
 userSchema.pre("save", async function(next) {
